a7d88b297de3d09da653d711c00f2b317c0645e6
Mai Dang Loi <loi.mai-dang@banvien.com.vn>
2025-05-09 11:50:53 +0700

    create docker compose

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT) || 5432,
+  port: parseInt(process.env.DB_PORT) || 5435,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || '123456',
   database: process.env.DB_NAME || 'metric',
